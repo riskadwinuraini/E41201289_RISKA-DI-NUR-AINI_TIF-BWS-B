854c1bd0156dc4fc44561d836721983f566550a8
riskadwinuraini <74886545+riskadwinuraini@users.noreply.github.com>
2021-03-16 20:17:15 +0700

    Add files via upload

---
+++ ProblemVariabel.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variabel;
+
+/**
+ *
+ * @author nizam
+ */
+public class ProblemVariabel {
+    public static void main(String[] args) {
+        
+        //deklarasi variabel yang benar
+        
+        boolean gameOver = false;
+        int students = 50, classes = 3;
+        double sales_tax;
+        short number1;
+        
+        deklarasi variabel yang salah
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+    }         
+}
